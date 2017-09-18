d7cd3ad84bb5d64db23117a00c31e19be4d9ebda
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-18 16:48:14 +0300

    added validation

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator/check')
 
 const Topic = require('../models/topics')
 
-router.get('/', (req, res) => {
-  Topic.find({}).then(entries => {
-    return res.json({ topics: entries })
-  }).catch(e => {
+router.get('/', async (req, res) => {
+  try {
+    const topics = await Topic.find({})
+    return res.json({ topics })
+  } catch (e) {
     console.error(e)
     return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  })
+  }
 })
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params
-
-  if (!id) return res.status(400)
-
   try {
+    const { id } = req.params
+    if (!id) return res.status(400)
     const topic = await Topic.findById(id)
     return res.json({ topic })
   } catch (e) {
@@ -26,10 +26,15 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.post('/', async (req, res) => {
-  const { name } = req.body
-
+router.post('/', [
+  body('name').exists().withMessage('Name must exist')
+  .isAlphanumeric().withMessage('Name must contain only letters and numbers')
+  .isLength({ min: 4 }).withMessage('Name must be at least 4 characters long')
+], async (req, res) => {
   try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
+    const { name } = req.body
     const newTopic = new Topic({ name })
     const topic = await newTopic.save()
     return res.status(201).send({ topic })
@@ -39,11 +44,17 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.put('/:id', async (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-
+router.put('/:id', [
+  body('name').exists().withMessage('Name must exist')
+  .isAlphanumeric().withMessage('Name must contain only letters and numbers')
+  .isLength({ min: 4 }).withMessage('Name must be at least 4 characters long')
+], async (req, res) => {
   try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
+
+    const { id } = req.params
+    const { name } = req.body
     await Topic.findByIdAndUpdate(id, { name })
     return res.status(200).send()
   } catch (e) {
@@ -53,9 +64,8 @@ router.put('/:id', async (req, res) => {
 })
 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
   try {
+    const { id } = req.params
     await Topic.findByIdAndUpdate(id, { deleted: new Date() })
     return res.status(200).send()
   } catch (e) {
