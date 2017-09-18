1d082cdf39cbccf5702c2a074ecbbfcce91c2f51
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-18 16:53:56 +0300

    added mongoose debug

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+mongoose.set('debug', true)
 
 const topics = require('./routes/topics')
 app.use('/api/topics', topics)
