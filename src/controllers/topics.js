1abdbbc9371c07bfb5379ce2deb40bde1c797d0f
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-25 15:06:35 +0300

    added asyncMiddleware

--- src/controllers/topics.js
+++ src/controllers/topics.js
@@ -1,58 +1,33 @@
 const Topic = require('../models/topics')
 
 module.exports.getTopics = async (req, res) => {
-  try {
-    const topics = await Topic.find({})
-    return res.json({ topics })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
+  const topics = await Topic.find({})
+  return res.json({ topics })
 }
 
 module.exports.getTopic = async (req, res) => {
-  try {
-    const { id } = req.params
-    if (!id) return res.status(400)
-    const topic = await Topic.findById(id)
-    return res.json({ topic })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
+  const { id } = req.params
+  if (!id) return res.status(400)
+  const topic = await Topic.findById(id)
+  return res.json({ topic })
 }
 
 module.exports.postTopic = async (req, res) => {
-  try {
-    const { name } = req.body
-    const newTopic = new Topic({ name })
-    const topic = await newTopic.save()
-    return res.status(201).send({ topic })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
+  const { name } = req.body
+  const newTopic = new Topic({ name })
+  const topic = await newTopic.save()
+  return res.status(201).send({ topic })
 }
 
 module.exports.putTopic = async (req, res) => {
-  try {
-    const { id } = req.params
-    const { name } = req.body
-    await Topic.findByIdAndUpdate(id, { name })
-    return res.status(200).send()
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
+  const { id } = req.params
+  const { name } = req.body
+  await Topic.findByIdAndUpdate(id, { name })
+  return res.status(200).send()
 }
 
 module.exports.deleteTopic = async (req, res) => {
-  try {
-    const { id } = req.params
-    await Topic.findByIdAndUpdate(id, { deleted: new Date() })
-    return res.status(200).send()
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
+  const { id } = req.params
+  await Topic.findByIdAndUpdate(id, { deleted: new Date() })
+  return res.status(200).send()
 }
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const validate = require('../utils/validate')
 const topics = require('../controllers/topics')
+const { asyncMiddleware } = require('../utils/common')
 
-router.get('/', topics.getTopics)
-router.get('/:id', topics.getTopic)
-router.post('/', validate.topic, topics.postTopic)
-router.put('/:id', validate.topic, topics.putTopic)
-router.delete('/:id', topics.deleteTopic)
+router.get('/', asyncMiddleware(topics.getTopics))
+router.get('/:id', asyncMiddleware(topics.getTopic))
+router.post('/', validate.topic, asyncMiddleware(topics.postTopic))
+router.put('/:id', validate.topic, asyncMiddleware(topics.putTopic))
+router.delete('/:id', asyncMiddleware(topics.deleteTopic))
 
 module.exports = router
