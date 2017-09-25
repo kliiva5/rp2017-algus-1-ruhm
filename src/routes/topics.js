bb6a091e5e356fa0b3d157029622d9bf1a0ece8f
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-25 14:56:02 +0300

    added separete controllers and validation

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,75 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { body, validationResult } = require('express-validator/check')
+const validate = require('../utils/validate')
+const topics = require('../controllers/topics')
 
-const Topic = require('../models/topics')
-
-router.get('/', async (req, res) => {
-  try {
-    const topics = await Topic.find({})
-    return res.json({ topics })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
-})
-
-router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    if (!id) return res.status(400)
-    const topic = await Topic.findById(id)
-    return res.json({ topic })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
-})
-
-router.post('/', [
-  body('name').exists().withMessage('Name must exist')
-  .isLength({ min: 4 }).withMessage('Name must be at least 4 characters long')
-], async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
-    const { name } = req.body
-    const newTopic = new Topic({ name })
-    const topic = await newTopic.save()
-    return res.status(201).send({ topic })
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
-})
-
-router.put('/:id', [
-  body('name').exists().withMessage('Name must exist')
-  .isLength({ min: 4 }).withMessage('Name must be at least 4 characters long')
-], async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
-
-    const { id } = req.params
-    const { name } = req.body
-    await Topic.findByIdAndUpdate(id, { name })
-    return res.status(200).send()
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
-})
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    await Topic.findByIdAndUpdate(id, { deleted: new Date() })
-    return res.status(200).send()
-  } catch (e) {
-    console.error(e)
-    return res.status(400).send({ error: { msg: 'something went wrong here' } })
-  }
-})
+router.get('/', topics.getTopics)
+router.get('/:id', topics.getTopic)
+router.post('/', validate.topic, topics.postTopic)
+router.put('/:id', validate.topic, topics.putTopic)
+router.delete('/:id', topics.deleteTopic)
 
 module.exports = router
